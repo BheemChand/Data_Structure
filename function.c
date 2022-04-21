e254f8e8001fb147780fc1f24d2663e703563a39
RachitMaheshwari <rachitmaheshwari829@gmail.com>
2022-04-21 23:02:25 +0530

    reverse program

---
+++ function.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int reverse()
+{
+int n, reverse=0, rem;    
+printf("Enter a number: ");    
+  scanf("%d", &n);    
+  while(n!=0)    
+  {    
+     rem=n%10;    
+     reverse=reverse*10+rem;    
+     n/=10;    
+  }    
+  printf("Reversed Number: %d",reverse);    
+
+return reverse;
+}
+int main()
+{
+    reverse();
+    return 0;
+}
